2beca791a3236b64de2931fac212f739c4d9fd41
Bruno Mendoza <hola@brunomendoza.es>
2022-02-13 17:32:06 +0100

    Add ticket DTO

---
+++ AareonTechnicalTest/DTO/TicketDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AareonTechnicalTest.DTO
+{
+    public class TicketDto
+    {
+        public int Id { get; }
+
+        public string Content { get; set; }
+
+        public int PersonId { get; set; }
+    }
+}
