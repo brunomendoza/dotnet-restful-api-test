fa12eea053549e089304a902e6a2cea52df20493
Bruno Mendoza <hola@brunomendoza.es>
2022-02-13 17:31:44 +0100

    Add ticket API controller

---
+++ AareonTechnicalTest/Controllers/TicketController.cs
@@ -0,0 +1,34 @@
+﻿using AareonTechnicalTest.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AareonTechnicalTest.Controllers
+{
+    [ApiController]
+    [Route("[controler]")]
+    public class TicketController : ControllerBase
+    {
+        [HttpGet(Name = "GetTicket")]
+        public TicketDto getTicket(int id)
+        {
+            return null;
+        }
+
+        [HttpPost(Name = "SaveTicket")]
+        public int saveTicket(TicketDto ticketDto)
+        {
+            return 0;
+        }
+
+        [HttpPut(Name = "UpdateTicket")]
+        public TicketDto updateTicket(TicketDto ticketDto)
+        {
+            return null;
+        }
+
+        [HttpDelete(Name = "DeleteTicket")]
+        public bool deleteTicket(TicketDto ticketDto)
+        {
+            return false;
+        }
+    }
+}
